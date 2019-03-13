4471f6484a44056a06d74f81332b7606467b1f8e
Yury <31337@mail.ru>
2019-03-13 16:52:08 +0400

    Listeners were added to the buttons

--- app/src/main/java/com/yurima/weightloser/MainActivity.java
+++ app/src/main/java/com/yurima/weightloser/MainActivity.java
@@ -1,13 +1,46 @@
 package com.yurima.weightloser;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_weight_activity)
+    Button weightActivityButton;
+    @BindView(R.id.btn_food_activity)
+    Button foodActivityButton;
+    @BindView(R.id.btn_graph_activity)
+    Button graphActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+    @OnClick({R.id.btn_graph_activity, R.id.btn_weight_activity, R.id.btn_food_activity})
+    public void openActivity(Button b){
+        Intent intent;
+        switch (b.getId()) {
+            case R.id.btn_weight_activity :
+                intent = new Intent(this, WeightActivity.class);
+                break;
+            case R.id.btn_food_activity :
+                intent = new Intent(this, FoodActivity.class);
+                break;
+            case R.id.btn_graph_activity :
+                intent = new Intent(this, GraphActivity.class);
+                break;
+            default:
+                return;
+        }
+    }
+
 }
