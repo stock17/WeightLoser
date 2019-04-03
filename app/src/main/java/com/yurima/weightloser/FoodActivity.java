5dd0ddf15edf2612a555b3f8cb005d2ad0573b25
Yury <31337@mail.ru>
2019-04-03 15:32:17 +0400

    added Insert Dialog

--- app/src/main/java/com/yurima/weightloser/FoodActivity.java
+++ app/src/main/java/com/yurima/weightloser/FoodActivity.java
@@ -51,9 +51,7 @@ public class FoodActivity extends AppCompatActivity {
         foodListRecyclerView.setAdapter(adapter);
     }
 
-    private void insertFood(String title, String unit, String value) {
-        mDbAdapter.insertFood(title, unit, value);
-    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -72,41 +70,85 @@ public class FoodActivity extends AppCompatActivity {
         }
 
         if (item.getItemId() == R.id.item_remove_food_table) {
+            onRemoveButtonClick();
+            return true;
+        }
 
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            View view = getLayoutInflater().inflate(R.layout.dialog_remove_item, null);
-            final EditText et = (EditText) view.findViewById (R.id.et_remove_food_table);
-
-            builder
-                    .setTitle("Remove Item")
-                    .setView(view)
-                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            String removeItem = et.getText().toString();
-                            removeItemFromTable(removeItem);
-                        }
-                    })
-                    .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    })
-            ;
-
-            AlertDialog dialog = builder.create();
-            dialog.show();
+        if (item.getItemId() == R.id.item_insert_food_table) {
+            onInsertButtonClick();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-       private void removeItemFromTable(String itemTitle) {
+    private void onRemoveButtonClick() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dialog_remove_item, null);
+        final EditText et = (EditText) view.findViewById (R.id.et_remove_food_table);
+
+        builder
+                .setTitle("Remove Item")
+                .setView(view)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String removeItem = et.getText().toString();
+                        removeItemFromTable(removeItem);
+                    }
+                })
+                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+        ;
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
+    private void onInsertButtonClick() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dialog_insert_item, null);
+        final EditText title = (EditText) view.findViewById (R.id.et_insert_food_title);
+        final EditText unit = (EditText) view.findViewById (R.id.et_insert_food_unit);
+        final EditText value = (EditText) view.findViewById (R.id.et_insert_food_value);
+
+        builder
+                .setTitle("Insert Item")
+                .setView(view)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String stringTitle = title.getText().toString();
+                        String stringUnit = unit.getText().toString();
+                        String stringValue = value.getText().toString();
+                        insertFood(stringTitle, stringUnit, stringValue);
+                    }
+                })
+                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+        ;
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void insertFood(String title, String unit, String value) {
+        mDbAdapter.insertFood(title, unit, value);
+        adapter.onChangeDataSet(mDbAdapter.getFoods());
+    }
+
+    private void removeItemFromTable(String itemTitle) {
         mDbAdapter.deleteFood(itemTitle);
         adapter.onChangeDataSet(mDbAdapter.getFoods());
-
     }
 
     @Override
